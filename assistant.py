6b2e81ce5c2a24952538b2da3ca66590d2b11d5e
tomazws <tomazws@mac.com>
2024-03-13 01:03:54 -0700

    test

--- assistant.py
+++ assistant.py
@@ -113,6 +113,8 @@ if prompt := st.chat_input('Ask me anything about CS 3186'):
         # Display assistant message in chat message container
         with st.chat_message('assistant'):
             st.markdown(reponse_message['content'])
+            if reponse_message['diagram'] != '':
+                st.graphviz_chart(reponse_message['diagram'])
 
         # Add assistant message to chat history
         st.session_state.messages.append(reponse_message)
