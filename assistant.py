2765d3b99aaf6963dca8dd2642379355c31e07cf
tomazws <tomazws@mac.com>
2024-03-18 12:21:57 -0700

    test

--- assistant.py
+++ assistant.py
@@ -78,14 +78,13 @@ with st.sidebar:
     st.write('Buttons')
     
 if st.sidebar.button('Convert NFA to DFA'):
-    displayMessage('user', 'Convert NFA to DFA')
+    #displayMessage('user', 'Convert NFA to DFA')
     st.session_state.messages.append({'role': 'user', 'content': 'Convert NFA to DFA'})
     getCompletion('Convert NFA to DFA')
 
 # Display chat messages
 for message in st.session_state.messages:
-    #displayMessage(message['role'], message['content'])
-    pass
+    displayMessage(message['role'], message['content'])
 
 # Chat input
 if prompt := st.chat_input('Ask me anything about CS 3186'):
