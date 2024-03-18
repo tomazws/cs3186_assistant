e5bb3dcaffe325b68009505932425886876d165f
tomazws <tomazws@mac.com>
2024-03-18 12:20:35 -0700

    test

--- assistant.py
+++ assistant.py
@@ -78,9 +78,9 @@ with st.sidebar:
     st.write('Buttons')
     
 if st.sidebar.button('Convert NFA to DFA'):
-    displayMessage('user', 'Convert NFA to DFA')
+    #displayMessage('user', 'Convert NFA to DFA')
     st.session_state.messages.append({'role': 'user', 'content': 'Convert NFA to DFA'})
-    #getCompletion('Convert NFA to DFA')
+    getCompletion('Convert NFA to DFA')
 
 # Display chat messages
 for message in st.session_state.messages:
