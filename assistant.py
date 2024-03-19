9d1bff6fa1f441fd5b1147a2fdbd1750f4a15a9f
tomazws <tomazws@mac.com>
2024-03-18 17:09:49 -0700

    test

--- assistant.py
+++ assistant.py
@@ -40,6 +40,7 @@ def displayMessage(role, content):
             match = re.search('digraph .FA {', message)
             if match and message[-2] == '}':
                 message = message[match.start():]
+                st.text(message)
                 st.graphviz_chart(message)
             else:
                 st.write(message)
